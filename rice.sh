9f18f95c4c31623953c448c6d814ebe173d5d04d
Kokossy <88337824+Kokossy@users.noreply.github.com>
2022-04-21 22:11:39 +0200

    Add files via upload

---
+++ rice.sh
@@ -0,0 +1,49 @@
+#add to repo: config bspwm, ocnfig sxhkd config xinitrc and /etc/xdg/picom.conf
+#tapeta mosi sie nazywac basic.jpg
+
+sudo pacman -S git base-devel
+cd ~
+mkdir Downloads
+mkdir Wallpapers
+cd Downloads
+  
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+yay -S mpd libmpdclient
+yay -S picom bspwm dunst polybar arandr rofi sxhkd nitrogen kitty rxvt-unicode neovim brave-bin nautilus rofi-dmenu lxappearance gedit gnome-disk-utility gnome-control-center alsa-utils sddm xorg-xsetroot calc pywal networkmanagwer-dmenu neofetch zsh ripgrep gnome-font-viewer nerd-fonts-fira-code nerd-fonts-hack noto-fonts-emoji nerd-fonts-jetbrains-mono spotify discord visual-studio-code-bin catppuccin-gtk-theme gnome-keyring cava cmatrix xdg-user-dirs-gtk
+
+cd ~
+mkdir .config/bspwm
+mkdir .config/sxhkd
+cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm
+cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd
+cp /etc/dunst/dunstrc ~/.config/dunst/dunstrc
+cp /etc/X11/xinit/xinitrc .xinitrc
+sudo systemctl enable sddm.service
+
+cd ~/Downloads
+git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
+cd polybar-themes
+chmod +x setup.sh
+./setup.sh
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+nvim +'hi NormalFloat guibg=#1e222a' + PackerSync
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/Kokossy/firstdotfiles
+cd firstdotfiles
+cp bspwm ~/.config/
+cp dunst ~/.config/
+cp picom ~/.config/
+cp rofi ~/.config/
+cp sxhkd ~/.config/
+cp .screenlayout ~/
+cp .xinitrc ~/
+
+echo "Install catppuccin themes! IMPORTANT: GTK, mouse"
+
+
